be6b10361b1538cf2c9688223eb68c491f44fc72
dongu54321 <12064071+dongu54321@users.noreply.github.com>
2023-09-20 20:12:53 +0700

    Update tailscale.sh

--- tailscale.sh
+++ tailscale.sh
@@ -1,3 +1,3 @@
 sudo apt update && apt install curl
 curl -fsSL https://tailscale.com/install.sh | sudo sh
-sudo tailscale up --login-server https://wg.momo.nohost.me/
+echo sudo tailscale up --login-server https://wg.momo.nohost.me/
